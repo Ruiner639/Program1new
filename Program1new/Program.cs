5d659d1fb60db9dbf6c40cf5fa33cd4200ce1e15
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-03 02:50:23 +0300

    Добавьте файлы проекта.

---
+++ Program1new/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program1new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I slightly modified the program :)
+            try
+            {
+                Console.WriteLine("Enter how old are you");
+                int years = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("In 10 years you will be " + (years + 10));
+            }
+            catch
+            {
+                Console.WriteLine("You entered an invalid value");
+            }
+        }
+    }
+}
